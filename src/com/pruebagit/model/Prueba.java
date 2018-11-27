00e8f84f93610c8b0a6cde568e833555e0d81641
AleCasGam <ale.castrogamboa12@gmail.com>
2018-11-27 17:45:41 -0600

    Get y Set

--- src/com/pruebagit/model/Prueba.java
+++ src/com/pruebagit/model/Prueba.java
@@ -1,5 +1,16 @@
 package com.pruebagit.model;
 
 public class Prueba {
+	private String idEmpleado;
+	
+	public String getIdEmpleado()
+	{
+		return idEmpleado;
+	}
+	
+	public void setIdEmpleado(String idEmpleado)
+	{
+		this.idEmpleado = idEmpleado;
+	}
 
 }
